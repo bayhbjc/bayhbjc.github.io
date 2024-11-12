00dc88aff9da45fb4ead5fdfbf650e683d6f9090
niko <zhangqibin@hhca2.wecom.com>
2024-11-12 20:26:43 +0800

    第五次

--- index/js/index.js
+++ index/js/index.js
@@ -1,1 +1,50 @@
 
+var $list = $('.list')
+var $nav = $('.nav')
+
+$(window).scroll(function () {
+	var scrollTop = $(window).scrollTop();
+	console.log(scrollTop);
+
+	
+
+
+	var list = $(".box2").find(".list");
+	// console.log(list);
+
+	list.each(function (index, item) {
+		// console.log(index, item);
+		var top = $(item).offset().top;
+		// console.log(top);
+
+		if (scrollTop >= top - 100) {
+			$(".nav ul li").eq(index).addClass("active").siblings().removeClass("active");
+
+		}
+	})
+
+})
+
+
+$('.nav li').click(function () {
+
+	if ($(this).hasClass('last')) {
+		// $(window).scrollTop(0)
+		$('html').animate({
+			scrollTop: 0
+		})
+
+		return
+	}
+
+	var idx = $(this).index()
+
+	var top = $list.eq(idx).offset().top
+
+	console.log($(window));
+
+
+	$("html").animate({
+		scrollTop: top
+	})
+})
